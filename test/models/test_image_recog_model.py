1139cea2438a49cada993f402a2f4a873cf6db8c
Annur Afgoni <afgoniannur@gmail.com>
2024-05-07 23:34:01 +0800

    feat: fix test_image_recog_model.py

--- test/models/test_image_recog_model.py
+++ test/models/test_image_recog_model.py
@@ -1,5 +1,13 @@
-from ..src.onepiece_classify.models import build_model
+import torch
+from src.onepiece_classify.models.build_model import image_recog
 
 def test_model():
+    rand_tensor = torch.rand([1, 3, 224, 224])
+    nclasses = 18
 
-    assert build_model.model() == torch.nn.Module
+    expected_output = (1, 18)
+
+    model = image_recog(num_classes=nclasses)
+
+    out = model.forward(rand_tensor)
+    assert out.shape == (1, 18)
